01f9cce89f184a4a20d427340e0cf169fbd15dad
Wassim Chakroun <wchakroun14@gmail.com>
2023-01-06 14:23:59 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 💎 Responsive World Envie Land Leagcy website
+
+- Landing page
+- Shop page & cart extension
+
+More to come...
---
+++ static/js/shop.js
@@ -0,0 +1,210 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== OPEN NAV MENU ===============*/
+let isScroll = false
+
+scrollSlide = () => {
+
+    if (isScroll) return
+
+    isScroll = true
+
+    let currProduct = document.querySelector('.product-info.active')
+    currProduct.classList.remove('active')
+    productIndex = productIndex + 1 > productInfos.length - 1 ? 0 : productIndex + 1
+    productInfos[productIndex].classList.add('active')
+
+    let  listitems = document.querySelectorAll('.slide')
+
+    let slider = document.querySelector('.slider')
+
+    let reverse = Array.from(listitems).slice().reverse()
+
+    left = reverse[0].offsetLeft+'px'
+    height = reverse[0].offsetHeight+'px'
+    width = reverse[0].offsetWidth+'px'
+    zIndex = reverse[0].style.zIndex
+
+    reverse.forEach((el, index) => {
+    
+        if (index < listitems.length-1) {
+            el.style.left = reverse[index + 1].offsetLeft+'px'
+            el.style.height = reverse[index + 1].offsetHeight+'px'
+            el.style.width = reverse[index + 1].offsetWidth+'px'
+            el.style.zIndex = index + 1
+            el.style.transform = 'unset'
+            el.style.opacity = '1'
+        }
+        if (index === listitems.length - 1) {
+            el.style.transform = 'scale(1.5)'
+            el.style.opacity = '0'
+
+            let cln = el.cloneNode(true)
+            
+            setTimeout(() => {
+                el.remove()
+                cln.style.transform = 'scale(0)'
+                cln.style.left = left
+                cln.style.height = height
+                cln.style.width = width
+                // cln.style.transform = 'unset'
+                cln.style.opacity = '0'
+                cln.style.zIndex = 0
+                cln.style.animation = 'unset'
+                slider.appendChild(cln)
+                isScroll = false
+            }, 1000);
+        }
+    })
+    listitems = document.querySelectorAll('.slide')
+    listitems[0].style.zIndex = '4'
+}
+
+let slideControl = document.querySelector('.slide-control')
+
+slideControl.onclick = (e) => {
+    scrollSlide()
+}
+
+openNav = () => {
+    let nav = document.querySelector('.nav-overlay')
+    let hamb = document.querySelector('.hamburger')
+    nav.classList.toggle('active')
+    hamb.classList.toggle('active')
+}
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+let testimonialSwiper = new Swiper(".testimonial-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+/*=============== NEW SWIPER ===============*/
+let newSwiper = new Swiper(".new-swiper", {
+    spaceBetween: 24,
+    loop: 'true',
+
+    breakpoints: {
+        576: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+    },
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SHOW CART ===============*/
+const cart = document.getElementById('cart'),
+      cartShop = document.getElementById('cart-shop'),
+      cartClose = document.getElementById('cart-close')
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+if(cartShop){
+    cartShop.addEventListener('click', () =>{
+        cart.classList.add('show-cart')
+    })
+}
+
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+if(cartClose){
+    cartClose.addEventListener('click', () =>{
+        cart.classList.remove('show-cart')
+    })
+}
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
